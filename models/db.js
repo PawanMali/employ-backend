0dfb6d5a7eb51ded3e8d7c0803d63948bf2563e7
PawanMali <51850335+PawanMali@users.noreply.github.com>
2019-07-15 13:23:51 +0530

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/EmployeeDB', { useNewUrlParser: true }, (err) => {
+mongoose.connect('mongodb+srv://dbadmin:Caavn0WU6vt278Jm@cluster0-ouigf.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true }, (err) => {
     if (!err) { console.log('MongoDB Connection Succeeded.') }
     else { console.log('Error in DB connection : ' + err) }
 });
